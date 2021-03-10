73708f043afe1295f25b319718f94c5198225862
Jack-Mac-21 <65779661+Jack-Mac-21@users.noreply.github.com>
2021-03-10 09:53:39 -0500

    Update scan.py

--- scan.py
+++ scan.py
@@ -9,21 +9,34 @@ import json
 output_dictionary = {}  # JSON dictionary as output where website is the key and dictionary of results is the value
 
 
+# Scanner takes a list of websites
+class Scanner:
+    def __init__(self, input_txt):
+        self.websites = input_txt
+        self.output = {}
+
+    def scan(self):
+        for site in self.websites:
+            key = site
+            value = {"scan-time: ": time.time()}
+            self.output.update({key: value})
+
+        json_dict = json.dumps(self.output, sort_keys=True, indent=4)
+        print(json_dict)
+
+
+# Takes the given input and reads it, modifies it and passes it to scanner
 def main():
-    input_file_name = sys.argv[1]
-    with open(input_file_name, 'r') as file:
+    input_txt = sys.argv[1]
+    with open(input_txt, 'r') as file:
         data = file.readlines()
 
+    list = []
     for site in data:
-        site = site.rstrip("\n")
-        key = site
-        value = {"scan_time: ": time.time()}
-        output_dictionary.update({key: value})
+        list.append(site.rstrip("\n"))
 
-    json_dict = json.dumps(output_dictionary, sort_keys=True, indent=4)
-    print(json_dict)
-    print("\n\n\nI made a change!")
-
+    scanner = Scanner(list)
+    scanner.scan()
 
 
 main()
